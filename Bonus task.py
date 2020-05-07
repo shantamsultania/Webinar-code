d6b8f46a6ef89e7f2e29eecc011d8a149d1ab9af
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2020-05-08 03:55:09 +0530

    Add files via upload

---
+++ Bonus task.py
@@ -0,0 +1,32 @@
+
+# ['Spider-Man','Captain America','Thor','Iron Man']
+
+# ['The Hulk','Black Panther','Doctor Strange']
+
+# create 2 list
+# name1 , name2 , name3
+
+# concatenate all the iteams in name3
+
+# add your name append it
+
+# now we need non magical super hero's so you will remove  'Doctor Strange'
+
+# 9 super hero's
+
+#Power_points = {'Spider-Man': 70 , 'Captain America': 70 ,
+#               'The Thor': 85,'Iron Man': 50,
+#               'The Hulk':70,'Black Panther': 66,'your super hero name here eg the coder ': 90}
+
+# find max Power points
+# Most_powerful_hero = max(Power_points,key = Power_points.get)
+
+# split first and last
+# first name
+# last name
+# full = last name + " " +first name
+# certificate name = full name.upper
+# coder the real name
+# email
+# email password
+
